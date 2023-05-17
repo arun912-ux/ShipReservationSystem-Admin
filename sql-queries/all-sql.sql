99861820a2794562b38b49edd6a6f3cf5a7afb0d
arun912-ux <arunshanagonda777@gmail.com>
2023-05-17 20:50:14 +0530

    added admin user

--- sql-queries/all-sql.sql
+++ sql-queries/all-sql.sql
@@ -4,6 +4,44 @@ use projects;
 show tables;
 
 
+
+
+
+# user admin
+
+CREATE TABLE users (
+    username VARCHAR(50) NOT NULL,
+    password VARCHAR(100) NOT NULL,
+    enabled TINYINT NOT NULL DEFAULT 1,
+    PRIMARY KEY (username)
+);
+
+CREATE TABLE authorities (
+    username VARCHAR(50) NOT NULL,
+    authority VARCHAR(50) NOT NULL,
+    FOREIGN KEY (username) REFERENCES users(username)
+);
+
+CREATE UNIQUE INDEX ix_auth_username
+    on authorities (username,authority);
+
+
+-- User user/pass
+INSERT INTO users (username, password, enabled)
+values ('admin', '$2a$10$8.UnVuG9HHgffUDAlk8qfOuVGkqRzgVymGe07xd00DMxs.AQubh4a', 1);
+
+INSERT INTO authorities (username, authority)
+values ('admin', 'ROLE_USER');
+
+
+
+
+
+
+
+
+
+
 # route-details
 insert into `route-details` (route_id, source, destination, distance) values (1, 'Kuala Bintang', 'Belyye Berega', 1341);
 insert into `route-details` (route_id, source, destination, distance) values (2, 'Aleksandrów', 'Nanxi', 1026);
